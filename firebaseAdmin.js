0dc3a95c1c4efe8497c682472173f4486f449421
Nicolas <nicolaspetcoff@gmail.com>
2024-11-06 13:51:27 -0300

    ajustes

--- firebaseAdmin.js
+++ firebaseAdmin.js
@@ -1,11 +1,28 @@
 // firebaseAdmin.js
 const admin = require("firebase-admin");
-const serviceAccount = require("./config/redmedia-aef35-firebase-adminsdk-hupym-a9e81f5b10.json");
+require('dotenv').config();
+
+// Verificar que FIREBASE_PRIVATE_KEY esté definida para evitar errores de configuración
+if (!process.env.FIREBASE_PRIVATE_KEY) {
+  console.error("Error: FIREBASE_PRIVATE_KEY no está definida en las variables de entorno.");
+  process.exit(1); // Termina el proceso si falta la clave privada
+}
 
 // Inicializar Firebase solo una vez
 if (!admin.apps.length) {
   admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
+    credential: admin.credential.cert({
+      type: process.env.FIREBASE_TYPE,
+      project_id: process.env.FIREBASE_PROJECT_ID,
+      private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+      private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'), // Formateo correcto para la clave privada
+      client_email: process.env.FIREBASE_CLIENT_EMAIL,
+      client_id: process.env.FIREBASE_CLIENT_ID,
+      auth_uri: process.env.FIREBASE_AUTH_URI,
+      token_uri: process.env.FIREBASE_TOKEN_URI,
+      auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_CERT_URL,
+      client_x509_cert_url: process.env.FIREBASE_CLIENT_CERT_URL,
+    }),
   });
 }
 
